dce714a9af34b041706245e03ac66fe8017113f8
Ben Zhang <ben.zhang@salesforce.com>
2024-05-09 22:48:07 -0700

    use satisifies to make it strong type when referred

--- src/configs/base.ts
+++ src/configs/base.ts
@@ -4,7 +4,8 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+import type { ClassicConfig } from '@typescript-eslint/utils/ts-eslint';
 
-export default {
+export = {
     plugins: ['@salesforce/lwc-mobile']
-};
+} satisfies ClassicConfig.Config;
--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -4,10 +4,11 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+import type { ClassicConfig } from '@typescript-eslint/utils/ts-eslint';
 
-export default {
+export = {
     extends: ['./configs/base'],
     rules: {
         '@salesforce/lwc-mobile/enforce-foo-bar': 'warn'
     }
-};
+} satisfies ClassicConfig.Config;
