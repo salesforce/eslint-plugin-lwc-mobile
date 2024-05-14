b47dbbc68b2babbf32a4c283337702901574aeaa
Ben Zhang <ben.zhang@salesforce.com>
2024-05-14 10:37:19 -0700

    signal type for plugin

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,18 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 
+import type { Linter } from '@typescript-eslint/utils/ts-eslint';
+
 import base from './configs/base.js';
 import recommended from './configs/recommended.js';
 import enforceFooBar from './rules/enforce-foo-bar.js';
+
 export = {
-    rules: {
-        'enforce-foo-bar': enforceFooBar
-    },
     configs: {
         base,
         recommended
+    },
+    rules: {
+        'enforce-foo-bar': enforceFooBar
     }
-};
+} satisfies Linter.Plugin;
