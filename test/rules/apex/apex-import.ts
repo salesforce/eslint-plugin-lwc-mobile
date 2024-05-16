10e3218456aa50620affaed559cde5387097f040
Takashi Arai <t.arai@salesforce.com>
2024-05-16 13:37:11 -0700

    Valid test.

--- test/rules/apex/apex-import.ts
+++ test/rules/apex/apex-import.ts
@@ -14,7 +14,30 @@ const ruleTester = new RuleTester({
 });
 
 ruleTester.run('@salesforce/lwc-mobile/apex/apex-import', apexImport, {
-    valid: [],
+    valid: [
+        {
+            code: `
+            import { LightningElement, wire } from 'lwc';
+            import getContactList from 'ContactController.getContactList';
+
+            export default class ApexWireMethodToFunction extends LightningElement {
+                contacts;
+                error;
+
+                @wire(getContactList)
+                wiredContacts({ error, data }) {
+                    if (data) {
+                        this.contacts = data;
+                        this.error = undefined;
+                    } else if (error) {
+                        this.error = error;
+                        this.contacts = undefined;
+                    }
+                }
+            }
+            `
+        }
+    ],
     invalid: [
         {
             code: `
