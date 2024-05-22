fe09c2cf131185cd0743bce1b31f3117ae31a3c9
Ben Zhang <ben.zhang@salesforce.com>
2024-05-22 15:27:33 -0700

    final touchup

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -5,11 +5,17 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 import type { ClassicConfig } from '@typescript-eslint/utils/ts-eslint';
+import { APEX_IMPORT_RULE_ID } from '../rules/apex/apex-import.js';
+import { NO_MUTATION_SUPPORTED_RULE_ID } from '../rules/graphql/no-mutation-supported';
+import { NO_AGGREGATE_QUERY_SUPPORTED_RULE_ID } from '../rules/graphql/no-aggregate-query-supported';
+import { UNSUPPORTED_SCOPE_RULE_ID } from '../rules/graphql/unsupported-scope.js';
+import { NO_SEMI_ANTI_JOIN_SUPPORTED_RULE_ID } from '../rules/graphql/no-semi-anti-join-supported';
+import { createFullRuleName } from '../util/createFullRuleName';
 
 export = {
     extends: ['./configs/base'],
     rules: {
-        '@salesforce/lwc-mobile/apex-import': 'warn'
+        [createFullRuleName(APEX_IMPORT_RULE_ID)]: 'warn',
     },
     overrides: [
         {
@@ -24,9 +30,10 @@ export = {
                 skipGraphQLConfig: true
             },
             rules: {
-                '@salesforce/lwc-mobile/offline-graphql-no-aggregate-query-supported': 'warn',
-                '@salesforce/lwc-mobile/offline-graphql-no-mutation-supported': 'warn',
-                '@salesforce/lwc-mobile/offline-graphql-unsupported-scope': 'warn'
+                [createFullRuleName(NO_MUTATION_SUPPORTED_RULE_ID)]: 'warn',
+                [createFullRuleName(NO_AGGREGATE_QUERY_SUPPORTED_RULE_ID)]: 'warn',
+                [createFullRuleName(UNSUPPORTED_SCOPE_RULE_ID)]: 'warn',
+                [createFullRuleName(NO_SEMI_ANTI_JOIN_SUPPORTED_RULE_ID)]: 'warn'
             }
         }
     ]
