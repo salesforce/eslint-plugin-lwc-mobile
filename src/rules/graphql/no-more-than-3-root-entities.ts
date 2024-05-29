4cc39c2cf7aab1c328083557b80bcc4cb8499834
Ben Zhang <ben.zhang@salesforce.com>
2024-05-28 17:34:41 -0700

    add copyright for files

--- src/rules/graphql/no-more-than-3-root-entities.ts
+++ src/rules/graphql/no-more-than-3-root-entities.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2024, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: MIT
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
+ */
+
 import { GraphQLESLintRule, GraphQLESLintRuleContext } from '@graphql-eslint/eslint-plugin';
 import getDocUrl from '../../util/getDocUrl';
 import { DocumentStat, ViolationType } from './EntityStats';
--- src/rules/graphql/no-mutation-supported.ts
+++ src/rules/graphql/no-mutation-supported.ts
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: MIT
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
+
 import { GraphQLESLintRule, GraphQLESLintRuleContext } from '@graphql-eslint/eslint-plugin';
 
 import { getLocation } from '../../util/graphqlAstUtils';
