0f8c5e8f8f65a42199741c9fcf9defec219464b4
haifeng.li <haifeng.li@salesforce.com>
2024-09-18 13:10:27 -0700

    test: modify test case

--- test/util/org.spec.ts
+++ test/util/org.spec.ts
@@ -1,3 +1,28 @@
+//import { ConfigAggregator, AuthInfo, Connection, StateAggregator } from '@salesforce/core';
+//import { OrgUtils } from '../../src/util/org-utils';
+// import fs from 'fs';
+// import path from 'path';
+//import AccountObjectInfo from '../mockData/object-Account.json';
+// jest.mock('@salesforce/core', () => {
+//     return {
+//         ConfigAggregator: {
+//             create: jest.fn()
+//         },
+//         OrgConfigProperties: {
+//             TARGET_ORG: 'target-org'
+//         },
+//         StateAggregator: {
+//             getInstance: jest.fn()
+//         },
+//         Connection: {
+//             create: jest.fn()
+//         },
+//         AuthInfo: {
+//             create: jest.fn()
+//         }
+//     };
+// });
+
 describe('testImport', () => {
     it('sample code', async () => {
         expect(1).toBe(1);
