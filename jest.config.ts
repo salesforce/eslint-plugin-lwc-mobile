eafa6e92990495f91975ce41a52d4df93bf329a4
haifeng.li <haifeng.li@salesforce.com>
2024-09-20 10:11:35 -0700

    test: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -24,10 +24,6 @@ const config: Config = {
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.ts', '!src/index.ts', '!src/configs/*.ts'],
     coverageDirectory: 'reports/coverage',
-    moduleNameMapper: {
-        '^@graphql-eslint/eslint-plugin/(.*)$':
-            '<rootDir>/node_modules/@graphql-eslint/eslint-plugin/$1'
-    },
     reporters: [
         'default',
         [
